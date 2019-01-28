02c0d524bf3d68b7eb948682d62a1726210ac32f
Atul Bansal <atulbansal4444@gmail.com>
2019-01-28 18:59:14 +0530

    2nd method added.

--- LinkList/MergeLinkedListSort.java
+++ LinkList/MergeLinkedListSort.java
@@ -25,3 +25,35 @@ class MergeInLinkedListSort
 		System.out.println(ll);
 	}
 }
+public class Solution {
+    public ListNode mergeTwoLists(ListNode A, ListNode B) {
+        ListNode dummyNode = new ListNode(0);
+        ListNode tail = dummyNode;
+        while(true)
+        {
+            if(A == null) 
+            { 
+                tail.next = B; 
+                break; 
+            } 
+            if(B == null) 
+            { 
+                tail.next = A; 
+                break; 
+            }
+            if(A.val>B.val)
+            {
+                tail.next = B;
+                B = B.next;
+                
+            }
+            else
+            {
+                tail.next = A;
+                A = A.next;
+            }
+            tail = tail.next;
+        }
+        return dummyNode.next;
+    }
+}
