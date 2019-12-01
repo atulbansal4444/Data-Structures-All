4caab94f24289af62587873142380e3848242496
Atul Bansal <30015420+atulbansal4444@users.noreply.github.com>
2019-12-01 21:36:32 +0530

    Update Empty A jar.java

--- Dynamic/Empty A jar.java
+++ Dynamic/Empty A jar.java
@@ -1,35 +1,33 @@
-/* package codechef; // don't place package name! */
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
-class Codechef
+class Code
 {
 	static HashMap<Integer,Integer> m = new HashMap<>();
 
-	static int f(int n) {
+	static int f(int n) 
+	{
 		Integer t1 = m.get(n-1);
 		Integer t2 = m.get(n-3);
 
-		if (t1==null) {
-		 	t1 = f(n-1); 
-		 }
-		if (t2==null) {
-		 	t2 = f(n-3); 
-		 }
-		 int t3;
-		 m.put(n,t1+t2);
+		if (t1==null) 
+		{
+			t1 = f(n-1); 
+		}
+		if (t2==null) 
+		{
+			t2 = f(n-3); 
+		}
+		int t3;
+		m.put(n,t1+t2);
 		t3 = t1+t2;
 
 		return t3;
-
-
 	}
+	
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
 		//m.put(0, 0);
 		m.put(1, 1);
 		m.put(2, 1);
